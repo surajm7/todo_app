f56a2d328888e0dfbc7658245b58f6830715ec79
unknown <guptasurajm07@gmaile.com>
2022-07-24 16:19:22 +0530

    todo project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Todo from './Component/Todo'
+export default class App extends Component {
+  render() {
+    return (
+      <Todo/>
+    )
+  }
+}
+
+
